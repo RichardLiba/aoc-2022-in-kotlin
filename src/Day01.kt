dbd176e2e8284616e2b1aae80110f8c73f63a830
Richard Liba <richard.liba@slovenskoit.sk>
2022-12-01 09:25:41 +0100

    day 1 solution.

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,10 +1,18 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
-    }
-
-    fun part2(input: List<String>): Int {
-        return input.size
+        var caloriesForElf: Int = 0
+        val caloriesByElves: MutableList<Int> = mutableListOf()
+        input.forEach {
+            if (it.isEmpty() || it.toIntOrNull() == null) {
+                // next elf
+                caloriesByElves.add(caloriesForElf)
+                caloriesForElf = 0
+            } else {
+                // add up calories
+                caloriesForElf += it.toInt()
+            }
+        }
+        return caloriesByElves.indexOf(caloriesByElves.max()) + 1
     }
 
     // test if implementation meets criteria from the description, like:
@@ -13,5 +21,4 @@ fun main() {
 
     val input = readInput("Day01")
     println(part1(input))
-    println(part2(input))
 }
