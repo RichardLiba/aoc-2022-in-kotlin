f1e3cb0d92fb8cad537fc782201cbebfa13a1334
Richard Liba <richard.liba@slovenskoit.sk>
2022-12-01 12:33:29 +0100

    part 2 impl

--- src/Day01.kt
+++ src/Day01.kt
@@ -19,10 +19,28 @@ fun main() {
         return max
     }
 
+    fun part2(input: List<String>): Int {
+        var caloriesForElf = 0
+        val caloriesByElves: MutableList<Int> = mutableListOf()
+        input.forEach {
+            if (it.isEmpty() || it.toIntOrNull() == null) {
+                // next elf
+                caloriesByElves.add(caloriesForElf)
+                caloriesForElf = 0
+            } else {
+                // add up calories
+                caloriesForElf += it.toInt()
+            }
+        }
+        return caloriesByElves.sortedDescending().take(3).sum()
+    }
+
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
     check(part1(testInput) == 28000)
 
     val input = readInput("Day01")
     println(part1(input))
+
+    println(part2(input))
 }
