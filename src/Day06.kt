0bcd25d89e4f82e0399b8ccf2c634653ce19e786
Richard Liba <libarichard@gmail.com>
2022-12-06 10:07:32 +0100

    day 6 finished

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,15 +1,31 @@
 fun main() {
 
-    fun part1(input: List<String>): String {
-        return ""
+    fun findFirstUniqueIndex(length: Int, input: String): Int {
+        var foundMarker = ""
+        input.forEachIndexed { index, c ->
+            val temp = foundMarker + c
+            if (temp.toSet().size == length && temp.length == length) {
+                return index + 1
+            } else {
+                if (foundMarker.length >= length - 1) {
+                    foundMarker = foundMarker.drop(1).plus(c)
+                } else {
+                    foundMarker += c
+                }
+            }
+        }
+        return -1
     }
 
-
-    fun part2(input: List<String>): String {
-        return ""
+    fun part1(input: String): Int {
+        return findFirstUniqueIndex(4, input)
     }
 
-    val input = readInput("Day06")
+    fun part2(input: String): Int {
+        return findFirstUniqueIndex(14, input)
+    }
+
+    val input = readInput("Day06").first()
     println(part1(input))
 
     println(part2(input))
