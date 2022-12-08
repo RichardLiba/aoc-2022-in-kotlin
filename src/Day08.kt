33fc67f14cd439dcf3a236832ae0de7fad7fc14e
Richard Liba <libarichard@gmail.com>
2022-12-08 22:49:59 +0100

    day 8 part 1

--- src/Day08.kt
+++ src/Day08.kt
@@ -1,6 +1,49 @@
 fun main() {
+    fun readGrid(input: List<String>): List<List<Int>> {
+        return input.map {
+            it.map { s -> s.digitToInt() }
+        }
+    }
+
+    fun visibleTrees(grid: List<List<Int>>): Int {
+        val visible = Array(grid.size) { BooleanArray(grid[0].size) }
+        val n = grid.size
+        val m = grid[0].size
+        for (i in grid.indices) {
+            var heighest = -1
+            var heighestOtherDir = -1
+            for (j in 0 until m) {
+                if (grid[i][j] > heighest) {
+                    heighest = grid[i][j]
+                    visible[i][j] = true
+                }
+                if (grid[i][m - 1 - j] > heighestOtherDir) {
+                    heighestOtherDir = grid[i][m - 1 - j]
+                    visible[i][m - 1 - j] = true
+                }
+            }
+        }
+        for (j in grid[0].indices) {
+            var heighest = -1
+            var heighestOtherDir = -1
+            for (i in 0 until n) {
+                if (grid[i][j] > heighest) {
+                    heighest = grid[i][j]
+                    visible[i][j] = true
+                }
+                if (grid[n - 1 - i][j] > heighestOtherDir) {
+                    heighestOtherDir = grid[n - 1 - i][j]
+                    visible[n - 1 - i][j] = true
+                }
+            }
+        }
+        return visible.sumOf { it.count { it } }
+    }
+
     fun part1(input: List<String>): Int {
-        return 0
+        val grid = readGrid(input)
+        val visibleTrees = visibleTrees(grid)
+        return visibleTrees
     }
 
     fun part2(input: List<String>): Int {
