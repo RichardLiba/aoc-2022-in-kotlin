a275aa5f7ce334040e031fe810250c6af7887b50
Richard Liba <libarichard@gmail.com>
2022-12-08 21:56:32 +0100

    day 8 start

---
+++ src/Day08.kt
@@ -0,0 +1,16 @@
+fun main() {
+    fun part1(input: List<String>): Int {
+        return 0
+    }
+
+    fun part2(input: List<String>): Int {
+        return 1
+    }
+
+    val input = readInput("Day08")
+    println(part1(input))
+    val inputTEst = readInput("Day08_test")
+    println(part1(inputTEst))
+
+    println(part2(input))
+}
